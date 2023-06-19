d3a4999a107dfa82ec71989645f394baef8df3e5
raphael347 <raphaelpaul1234>
2023-06-19 17:57:56 +0200

    chn

--- middleware/notfoundroute.js
+++ middleware/notfoundroute.js
@@ -1,5 +1,5 @@
 const notfound=(req,res) => {
-    res.status(404).json(`Route not found try <a href='/api/v1/teams'>postit</a>`)
+    res.status(404).json(`Route not found try <a href='/api/v1/story'>postit</a>`)
 }
 
 module.exports =notfound
